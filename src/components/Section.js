b8883924456a78e50672578e8627e84301de0fcc
phildehovre <66724307+phildehovre@users.noreply.github.com>
2022-07-26 20:20:11 +0100

    3d effect first try angle not quite right

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Section.css'
 
 const Section = (props) => {
 
     const { children, bg, title, content, textColor, subtitle, id } = props
+
+    const [angleY, setAngleY] = useState(0)
+    const [angleX, setAngleX] = useState(0)
+    const [perspective, setPerspective] = useState(750)
+
+
+    // window.addEventListener('pointermove', (e) => {
+    //     let horizontalOffset = e.pageX;
+    //     let verticalOffset = e.pageY;
+    //     let hypothenuse = Math.sqrt(Math.pow(perspective, 2) + Math.pow(horizontalOffset, 2))
+    //     setAngleY((Math.acos(horizontalOffset / hypothenuse)) * - 57.2958 / 3)
+    //     setAngleX((Math.acos(verticalOffset / hypothenuse)) * 57.2958 / 3)
+    // })
+
+    window.addEventListener('click', (e) => {
+        console.log(e.clientX - e.pageX / 2, e.pageY - e.screenY / 2)
+    })
 
 
     const styles = {
@@ -13,8 +30,12 @@ const Section = (props) => {
 
     const cardStyle = {
         color: textColor,
-        borderColor: textColor
+        borderColor: textColor,
+        transform: `perspective(${perspective}px) 
+        rotateY(${angleY}deg) 
+        rotateX(${angleX}deg)`
     }
+
 
     return (
         <div data-id={id} style={styles} className='section-ctn'>
