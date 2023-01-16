0befe8389cb2bf7a4bdeed7ecf8c8c6ee50d18d2
phildehovre <66724307+phildehovre@users.noreply.github.com>
2023-01-16 15:29:47 +0000

    cv change 2023

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -50,6 +50,7 @@ const Homepage = (props) => {
                     'English, French, Dutch, Spanish',
                     'Musician',
                 ]}
+                linkToResume='https://res.cloudinary.com/dtnif6mzm/image/upload/v1673882874/Portfolio%20-%202022/Philippe_De_Hovre_-_Resume_2023_hyk0x0.png'
                 openModal={openModal}
             />
             <About
